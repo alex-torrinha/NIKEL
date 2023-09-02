95867f9559d9a4456abcb3bfcca8490d22f108f4
alexandre daniel torrinha <alexandre15otk@gmail.com>
2023-09-02 18:54:55 -0300

    iniciando o GIT

---
+++ public/js/base.js
@@ -0,0 +1,33 @@
+let pessoaDefault = {
+    nome: "alexandre",
+    idade: "24 anos",
+    trabalho: "programador",
+    }
+
+    let nomes = ["alexandre", "stella", "carol"]
+    let pessoas =  
+
+    function recebeEalteraNome (novoNome) {
+        nome = novoNome;
+        console.log("valor alterado recebendo um nome:");
+        console.log(nome);
+    }
+
+    function imprimirPessoa(pessoa) {
+        console.log("nome:");
+        console.log(pessoa.nome);
+
+        console.log("idade:");
+        console.log(pessoa.idade);
+
+        console.log("trabalho:");
+        console.log(pessoa.trabalho);
+    }
+
+    //imprimirPessoa(pessoaDefault);
+
+    //imprimirPessoa({
+        nome: "Julia Stella",
+    idade: "1 ano",
+    trabalho: "bebe da mamãe",
+    });
